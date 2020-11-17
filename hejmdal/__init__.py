6810e8752c9acba5f8c04dcd3ec78e346927742d
asknevegamby <aknvg@space.dtu.dk>
2020-11-17 16:32:28 +0100

    moved over updated __init__ and updated requirements.txt to include the extra tools to use for documentation and testing

--- hejmdal/__init__.py
+++ hejmdal/__init__.py
@@ -1,5 +1,9 @@
 from .base import *
 
+__all__ =  ['base', 'formatting', 'forms', 'datacalls', 'operations', 'main']
+
+__version__ = "0.1.1"
+
 from . import formatting
 from . import forms
 from . import datacalls
