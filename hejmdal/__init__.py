1b14ced05c1bdfa47ef891966aeefd8c38c3daf0
asknevegamby <aknvg@space.dtu.dk>
2020-11-19 11:32:16 +0100

    attempt by moving install_requires to setup.py from setup.cfg

--- hejmdal/__init__.py
+++ hejmdal/__init__.py
@@ -2,7 +2,7 @@ from .base import *
 
 __all__ =  ['base', 'formatting', 'forms', 'datacalls', 'operations', 'main']
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from . import formatting
 from . import forms
